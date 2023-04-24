cafa55c88501e2d832278b5f1dd9c8c08813b4f7
1Dire <d01038145300@nate.com>
2023-04-24 09:08:05 +0900

    feat : gradle버전 변경 , Dto추가

---
+++ src/main/java/com/example/springboot/web/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.springboot.web;
+
+import com.example.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// 컨트롤러를 JSON 을 반환하는 컨트롤러를 만들어줌
+//예전에는 @ResponseBody 를 각 메소드 마다 선언했던 것을 한번에 사용할 수 있게해준다고 생각하면됨
+
+public class HelloController {
+
+    // HTTP Method 인 Get 의 요청을 받을 수 있는 API 를 만들어줌
+    //예전에는 @RequestMapping(method = RequestMethod.GET) 으로 사용되었습니다.
+    //이제 이 프로젝트는 /hello 로 요청이오면 문자열 hello 를 반환하는 기능을 가지게 되었습니다.
+
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+}
